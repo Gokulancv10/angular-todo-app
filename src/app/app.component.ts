fdd1690f3373e7bcb99f5bd764be5141f7d9b854
gokulan <gokulan.v@jouve.com>
2022-08-06 13:51:02 +0530

    configure components in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoClass } from './todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo-app';
+
+  todoItems: TodoClass[] = [];
+
+  onCreateTodo(todoData: { id: number, name: string }) {
+    this.todoItems.push({
+      id: todoData.id,
+      name: todoData.name
+    })
+    console.log("AFTER CREATED TODO: ");
+    console.table(this.todoItems);
+  }
+
+  onDeleteTodo(todoId: { id: number }) {
+    console.log("DELETE TODO ID: " + todoId.id);
+    for (let index = 0; index < this.todoItems.length; index++) {
+      if (this.todoItems[index].id === todoId.id) {
+        this.todoItems.splice(index, 1);
+      }
+    }
+    console.log("AFTER DELETED A TODO: ");
+    console.table(this.todoItems);
+  }
+
+  onEditTodo(todoData: { id: number, updatedName: string }) {
+    for (let index = 0; index < this.todoItems.length; index++) {
+      if (this.todoItems[index].id === todoData.id) {
+        this.todoItems[index].name = todoData.updatedName;
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoItemsComponent } from './todo-items/todo-items.component';
+import { TodoCreateComponent } from './todo-create/todo-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemsComponent,
+    TodoCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
