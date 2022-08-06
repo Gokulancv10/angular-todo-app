ed51ca84967c6587af9668132031d0c220f44302
gokulan <gokulan.v@jouve.com>
2022-08-06 13:50:32 +0530

    create a component for display todo items

---
+++ src/app/todo-items/todo-items.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-items',
+  templateUrl: './todo-items.component.html',
+  styleUrls: ['./todo-items.component.css']
+})
+export class TodoItemsComponent implements OnInit {
+  updatedTodoName: string = '';
+
+  @Input()
+  todoItem!: { id: number, name: string };
+
+  @Output() todoEdited = new EventEmitter<{ id: number, updatedName: string }>();
+
+  @Output() todoDeleted = new EventEmitter<{ id: number }>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  editTodo(todoId: number) {
+    this.todoEdited.emit({
+      id: todoId,
+      updatedName: this.updatedTodoName
+    })
+    this.updatedTodoName = '';
+    console.table("EDITED TODO ID IS: " + todoId);
+  }
+
+  deleteTodo(todoId: number) {
+    this.todoDeleted.emit({ id: todoId });
+    console.log("DELETED TODO ID IS: " + todoId);
+  }
+}
